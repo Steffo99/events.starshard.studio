fae8595ed415cecfc6470366e3bf1fcc730f961b
Stefano Pigozzi <me@steffo.eu>
2023-03-28 19:41:39 +0200

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/base.root.css");
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/classic.root.css");
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/glass.root.css");
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/colors-purplestar.root.css");
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/fonts-fira-ghpages.root.css");
+	eleventyConfig.addPassthroughCopy("./bluelib-custom.root.css");
+};
