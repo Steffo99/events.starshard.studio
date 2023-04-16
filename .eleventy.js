838e49c78d5e7313499438409ac2bc8193c231df
Stefano Pigozzi <me@steffo.eu>
2023-04-16 04:08:58 +0200

    Upgrade bluelib to 8.1.0

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/base.root.css");
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/classic.root.css");
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/glass.root.css");
+	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/layouts-center.root.css");
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/colors-purplestar.root.css");
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/fonts-fira-ghpages.root.css");
 	eleventyConfig.addPassthroughCopy("./index.css");
