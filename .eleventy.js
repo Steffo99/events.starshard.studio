75ddf599fb1be7bf6f8652fb814cfeb3cb46ee23
Stefano Pigozzi <me@steffo.eu>
2023-04-16 04:09:10 +0200

    Make background load a smaller image if possible

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,10 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist/fonts-fira-ghpages.root.css");
 	eleventyConfig.addPassthroughCopy("./index.css");
 	eleventyConfig.addPassthroughCopy("./index.js");
+	eleventyConfig.addPassthroughCopy("./media/kazuend-2KXEb_8G5vo-unsplash-edited-full.jpg");
+	eleventyConfig.addPassthroughCopy("./media/kazuend-2KXEb_8G5vo-unsplash-edited-1080.jpg");
+	eleventyConfig.addPassthroughCopy("./media/kazuend-2KXEb_8G5vo-unsplash-edited-720.jpg");
+	eleventyConfig.addPassthroughCopy("./media/kazuend-2KXEb_8G5vo-unsplash-edited-480.jpg");
 
 	eleventyConfig.addFilter("flagEmoji", function(value) {
 		return {
