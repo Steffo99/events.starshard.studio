dec0168fa4be2fd92791a74ec537dd4cd2dd0c06
Stefano Pigozzi <me@steffo.eu>
2023-04-22 04:53:31 +0200

    Revert "Make raw JSON data available"
    It breaks autoreload

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./node_modules/@steffo/bluelib/dist");
 	eleventyConfig.addPassthroughCopy("./_tweaks");
     eleventyConfig.addPassthroughCopy("./_media");
-    eleventyConfig.addPassthroughCopy("./_data");
+    // eleventyConfig.addPassthroughCopy("./_data");
 
     // https://dev.to/jorik/country-code-to-flag-emoji-a21
 	eleventyConfig.addFilter("emojiFlag", function(value) {
